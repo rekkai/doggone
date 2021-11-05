867088708cca157df2f0c341449a4cc403309a98
Ayana Bando <ayanabando@gmail.com>
2021-11-05 12:05:32 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/doggone/MainActivity.kt
+++ app/src/main/java/com/example/doggone/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        getRandomDogPhoto()
     }
 
     fun getRandomDogPhoto() {
